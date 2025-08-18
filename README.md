2cf354d124fe7d42189492a5ef72a31c30e62378
MimiMc4 <miguelmarclave@gmail.com>
2025-08-18 14:09:29 +0200

    added file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # brightness
 Really simple brightnessctl wrapper to easily manage the brightness of my monitor in day to day use.
+
+If you want to use it just use brightnessctl -l to get your device name and change it in the script.
---
+++ brightness.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Easy screen brightness control
+
+# Configure your device name
+# Run "brightnessctl -l" to get list of devices
+DEVICE_NAME="intel_backlight"  
+
+max=$( brightnessctl -d "$DEVICE_NAME" m ) || {
+    echo "The device $DEVICE_NAME doesn't exist or brightnessctl is not installed." >&2
+    exit 1
+}
+current=$( brightnessctl -d "$DEVICE_NAME" g ) || exit 1
+step=$(( max / 10 ))
+
+case "$1" in
+    "++")
+        new=$((current + step < max ? current + step : max))
+        brightnessctl -d "$DEVICE_NAME" s "$new" 
+        ;;
+    "--")
+        new=$((current - step > 0 ? current - step : 0))
+        brightnessctl -d "$DEVICE_NAME" s "$new"
+        ;;
+    *)
+        if [[ $1 =~ ^[0-9]+$ && $1 -ge 0 && $1 -le 100 ]]; then
+            level=$(( $1 * max / 100 ))
+            brightnessctl -d "$DEVICE_NAME" s "$level" || echo "❌ ¿Necesitas sudo?" >&2
+        else
+            echo "Usage: brightness [ ++ | -- | 0-100 ]" >&2
+            exit 1
+        fi
+        ;;
+esac
+
